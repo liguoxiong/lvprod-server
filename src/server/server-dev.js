f95dc8e28e85da84a18301c14f96ee676fc67cb2
liguoxiong <bunyhung@gmail.com>
2019-09-16 23:20:03 +0700

    update server prod

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,4 +1,4 @@
-// import path from "path";
+import path from "path";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -9,13 +9,13 @@ import config from "../../webpack.dev.config.js";
 import routes from "./routes";
 import connectDb from "./../configs/db";
 const app = express(),
-  // DIST_DIR = __dirname,
-  // HTML_FILE = path.join(DIST_DIR, "index.html"),
+  DIST_DIR = __dirname,
+  HTML_FILE = path.join(DIST_DIR, "index.html"),
   compiler = webpack(config);
 
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
@@ -27,16 +27,16 @@ app.use("/api/categories", routes.category);
 app.use("/api/products", routes.product);
 app.use("/api/photos", routes.image);
 app.use("/api/routes", routes.sidebarList);
-// app.get("*", (req, res, next) => {
-//   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     res.set("content-type", "text/html");
-//     res.send(result);
-//     res.end();
-//   });
-// });
+app.get("/admin", (req, res, next) => {
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.set("content-type", "text/html");
+    res.send(result);
+    res.end();
+  });
+});
 // const eraseDatabaseOnSync = true;
 const PORT = process.env.PORT || 8080;
 connectDb();
--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -3,19 +3,28 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
-import connectDb from './../configs/db';
+import connectDb from "./../configs/db";
 
 const app = express(),
   DIST_DIR = __dirname,
-  HTML_FILE = path.join(DIST_DIR, "index.html");
+  ADMIN_HTML_FILE = path.join(DIST_DIR, "admin/index.html"),
+  HOME_HTML_FILE = path.join(DIST_DIR, "home/index.html");
 app.use(express.static(DIST_DIR));
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-app.get("*", (req, res) => {
-  res.sendFile(HTML_FILE);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+app.get("/admin", (req, res) => {
+  res.sendFile(ADMIN_HTML_FILE);
+});
+app.get("/", (req, res) => {
+  res.sendFile(HOME_HTML_FILE);
 });
 app.use("/api/user", routes.user);
+app.use("/api/categories", routes.category);
+app.use("/api/products", routes.product);
+app.use("/api/photos", routes.image);
+app.use("/api/routes", routes.sidebarList);
+
 const PORT = process.env.PORT || 8080;
 connectDb();
 app.listen(PORT, () => {
