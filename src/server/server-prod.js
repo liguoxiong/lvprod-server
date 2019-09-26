8066bb913f00025794d24a8b43406eb7ba5e7907
hunglq <hunglq@vietcas.vn>
2019-09-26 12:36:18 +0700

    build update

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -1,4 +1,5 @@
 import path from "path";
+import prerenderNode from 'prerender-node'
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -11,6 +12,7 @@ const app = express(),
   HOME_HTML_FILE = path.join(DIST_DIR, "home/index.html");
 app.use(express.static(DIST_DIR));
 app.use(cors());
+app.use(prerenderNode);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.get("/admin/*", (req, res) => {
