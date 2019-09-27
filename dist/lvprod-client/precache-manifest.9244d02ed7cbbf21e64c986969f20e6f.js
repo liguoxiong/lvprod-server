c5a241bb10ffe52b07ee487d7965bb3ba5b9a836
hunglq <hunglq@vietcas.vn>
2019-09-27 16:01:19 +0700

    update build

---
+++ dist/lvprod-client/precache-manifest.9244d02ed7cbbf21e64c986969f20e6f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171217dc8c32fbb0b0be077ac3dd0f39",
+    "url": "/lvprod-client/index.html"
+  },
+  {
+    "revision": "d7314012e8e3660e05fb",
+    "url": "/lvprod-client/static/css/2.ef41b40c.chunk.css"
+  },
+  {
+    "revision": "45efc89c892e16a71e33",
+    "url": "/lvprod-client/static/css/main.8f18426f.chunk.css"
+  },
+  {
+    "revision": "d7314012e8e3660e05fb",
+    "url": "/lvprod-client/static/js/2.17d4bcf8.chunk.js"
+  },
+  {
+    "revision": "45efc89c892e16a71e33",
+    "url": "/lvprod-client/static/js/main.db58500c.chunk.js"
+  },
+  {
+    "revision": "f83df487ac0b61e9d830",
+    "url": "/lvprod-client/static/js/runtime~main.85a80587.js"
+  },
+  {
+    "revision": "f4c2b29bd9993ed857675ad8c4d2d333",
+    "url": "/lvprod-client/static/media/facebook.f4c2b29b.svg"
+  },
+  {
+    "revision": "11f385398dd2f79985c92d936bf84df3",
+    "url": "/lvprod-client/static/media/messenger.11f38539.svg"
+  },
+  {
+    "revision": "b7d9c06fffa085ed40da9256a30d2170",
+    "url": "/lvprod-client/static/media/skype.b7d9c06f.svg"
+  },
+  {
+    "revision": "cf1c5d3ec16ad14ccc7a41bc37be7799",
+    "url": "/lvprod-client/static/media/viber.cf1c5d3e.svg"
+  },
+  {
+    "revision": "7d315d4a348c43339a371f4fbe6f0115",
+    "url": "/lvprod-client/static/media/zalo.7d315d4a.svg"
+  }
+]);
--- dist/lvprod-client/service-worker.js
+++ dist/lvprod-client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lvprod-client/precache-manifest.60e6ec43a67d9d0db6e899852d1a8cd9.js"
+  "/lvprod-client/precache-manifest.9244d02ed7cbbf21e64c986969f20e6f.js"
 );
 
 self.addEventListener('message', (event) => {
