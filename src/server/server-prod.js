3e5cbd8226db709b28bf3a2cce2c6b9164ac0d21
liguoxiong <bunyhung@gmail.com>
2019-10-06 16:10:57 +0700

    update

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -1,5 +1,4 @@
 import path from "path";
-import prerenderNode from "prerender-node";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
@@ -12,7 +11,9 @@ const app = express(),
   HOME_HTML_FILE = path.join(DIST_DIR, "index.html");
 app.use(express.static(DIST_DIR));
 app.use(cors());
-app.use(prerenderNode);
+app.use(
+  require("prerender-node").set("prerenderToken", "i8cyfM0flaG7Ls7lNrHR")
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.get("/admin/*", (req, res) => {
